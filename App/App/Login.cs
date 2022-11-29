6bec6c9244686c3002a54468100ea4d7faa18f03
Guilherme Uehara Piqui <uehara.piqui@gmail.com>
2022-11-28 21:16:27 -0300

    TP04

---
+++ App/App/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
